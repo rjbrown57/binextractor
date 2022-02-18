2f7e8228709df1b1528c6ca408f264866b24d145
rjrown57 <rjbrown57@gmail.com>
2022-02-18 11:17:12 -0600

    (fix): Make help message appear when no args passed

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -46,8 +31,8 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&sourcePath, "sourcePath", "s", "", "Path in binary to extract from")
 	rootCmd.PersistentFlags().StringVarP(&destinationPath, "destinationPath", "d", "", "Destination to write extracted binary to")
 
-	rootCmd.MarkFlagRequired("image")
-	rootCmd.MarkFlagRequired("sourcePath")
-	rootCmd.MarkFlagRequired("destinationPath")
+	rootCmd.MarkPersistentFlagRequired("image")
+	rootCmd.MarkPersistentFlagRequired("sourcePath")
+	rootCmd.MarkPersistentFlagRequired("destinationPath")
 
 }
